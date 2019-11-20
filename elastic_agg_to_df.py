accf4c494eea74bd1faa14aee8c4a337d6e75a0b
TsabarM <tsabar88@gmail.com>
2019-11-20 15:54:59 +0200

    get data - aggregated data from elastic elastic_agg_to_df = support functions for get data - generic flattening elastic data options - dictionaries for selected features

--- elastic_agg_to_df.py
+++ elastic_agg_to_df.py
@@ -7,38 +7,88 @@ Created on Mon Nov 18 20:47:54 2019
 
 import pandas as pd
 import json
-from pandas.io.json import json_normalize
-import itertools
-x=json.load(open(r'C:\Users\Tsabar\Documents\projects\anomaly sapir\try\try_elastic.json'))
+from elasticsearch_dsl.utils import AttrDict, AttrList
 
-X=json_normalize(x)
-
-X=json_normalize(X['aggregations.2.buckets'])
-X.columns
-
-time_key = '2'
-hosts_key = '3'
 
 def process_row(time, row):
     host = row['key']
     row_data = { 'time': time, 'host': host }
-    for key in row:
-        if key == 'key' or key == 'doc_count': continue
+    keys = [x for x in row if x != 'key' and x != 'doc_count']
+    if len(keys) == 1 and 'buckets' in row[keys[0]]:
+        print(f'found bucket key: {keys[0]}')
+        row_data
+    for key in keys:
         for value in row[key]:
-            if isinstance(row[key][value], dict): continue
-            row_data[key + '_' + value] = row[key][value]
+            if isinstance(row[key][value], AttrDict): continue
+            if isinstance(row[key][value], AttrList):
+                for item in row[key][value]:
+                    row_data[f"{key}_{item['key']}"] = item['value']
+            else:
+                row_data[key + '_' + value] = row[key][value]
     return row_data
 
-def process_time_bucket(time_bucket):
+
+def process_time_bucket(time_bucket, hosts_key):
     time = time_bucket['key']
     bucket_data = time_bucket[hosts_key]['buckets']
     rows = [process_row(time, x) for x in bucket_data]
     return rows
     
 
-time_buckets = x['aggregations'][time_key]['buckets']
-rows = [process_time_bucket(x) for x in time_buckets]
-flat_rows = [y for x in rows for y in x]
+def build_aggregation_dataframe(x, time_key='2', hosts_key='3'):
+    time_buckets = x['aggregations'][time_key]['buckets']
+    rows = [process_time_bucket(x, hosts_key) for x in time_buckets]
+    flat_rows = [y for x in rows for y in x]
+    df = pd.DataFrame(flat_rows)
+    return df
 
-df= pd.DataFrame(flat_rows)
-time_temp=pd.to_datetime(df['time']*1000000)
+
+def get_bucket(row):
+    keys = [x for x in row if x != 'key' and x != 'key_as_string' and x != 'doc_count']
+    is_bucket = len(keys) == 1 and 'buckets' in row[keys[0]]
+    bucket_key = keys[0] if is_bucket else None
+    return is_bucket, bucket_key
+
+
+def process_generic_row(rows, node, bucket_key=None, row_data=None):
+    if row_data is None:
+        row_data = {}
+    if bucket_key:
+        row_data[bucket_key] = node['key']
+    keys = [x for x in node if x != 'key' and x != 'doc_count']
+    for key in keys:
+        for value in node[key]:
+            if isinstance(node[key][value], AttrDict) or isinstance(node[key][value], dict): continue
+            if isinstance(node[key][value], AttrList) or isinstance(node[key][value], list):
+                for item in node[key][value]:
+                    row_data[f"{key}_{item['key']}"] = item['value']
+            else:
+                row_data[key + '_' + value] = node[key][value]
+    rows.append(row_data)
+
+
+def build_generic_bucket(rows, list, bucket_key, row_data=None):
+    for node in list:
+        node_row_data = {} if row_data is None else row_data.copy()
+        node_row_data[bucket_key] = node['key']
+        is_bucket, inner_bucket_key = get_bucket(node)
+        if is_bucket:
+            build_generic_bucket(rows, node[inner_bucket_key]['buckets'], inner_bucket_key, node_row_data)
+        else:
+            process_generic_row(rows, node, inner_bucket_key, node_row_data)
+
+
+def build_generic_aggregations_dataframe(x):
+    rows = []
+    node = x['aggregations']
+    is_bucket, bucket_key = get_bucket(node)
+    build_generic_bucket(rows, node[bucket_key]['buckets'], bucket_key)
+    df = pd.DataFrame(rows)
+    return df
+
+
+if __name__ == "__main__":
+    x=json.load(open(r'C:\Users\Tsabar\Documents\projects\anomaly sapir\try\try_elastic.json'))
+    # df = build_aggregation_dataframe(x)
+    # time_temp=pd.to_datetime(df['time']*1000000)
+    print(build_generic_aggregations_dataframe(x))
--- get_data.py
+++ get_data.py
@@ -2,6 +2,11 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 import argparse
 import json
+from elastic_agg_to_df import build_generic_aggregations_dataframe
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
 def get_hit_dict(hit):
@@ -23,8 +28,22 @@ def download_json(path, index, last_minutes, host, port, user, password):
         }
     }
     search = search.query('range', **time_query)
-    total = search.count()
-    print(total)
+    aggs = search.aggs.bucket('timestamp', 'date_histogram', field="@timestamp", interval="1m")
+    aggs = aggs.bucket('host', 'terms', field="host.name.keyword", size=1000000000)
+    aggs = aggs.bucket('city', 'terms', field="city.keyword", size=1000000000)
+    aggs.metric('idle_sum', 'sum', field="system.cpu.idle.pct")
+    aggs.metric('cpu_stats', 'extended_stats', field="system.cpu.system.pct")
+    aggs.metric('cpu_steal', 'percentiles', field="system.cpu.steal.pct", percents=[25, 75], keyed=False)
+    # search.query('range', **time_query)
+    # total = search.count()
+    # print(total)
+    response = search.execute()
+    print("test")
+    print(response.aggregations.to_dict())
+    df = build_generic_aggregations_dataframe(response)
+    print(df.head(40))
+    return
+    
     results = [get_hit_dict(hit) for hit in search.scan()]
 
     print('downloaded')
@@ -35,9 +54,9 @@ def download_json(path, index, last_minutes, host, port, user, password):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download elastic search last X minutes on an index')
-    parser.add_argument('--path', '-o', help='JSON output path', type=str, default="C:/Users/Tsabar/projects/monitor/json/aws_141119_1315.json", required=False)
+    parser.add_argument('--path', '-o', help='JSON output path', type=str, default="C:/Users/Tsabar/projects/monitor/json/aws_17_19112019.json", required=False)
     parser.add_argument('--index', '-i', help="ElasticSearch index", type=str, default='metricbeat-*', required=False)
-    parser.add_argument('--lastMinutes', '-m', help="Last minutes to get data for", dest='last_minutes', type=int, default=0, required=False)
+    parser.add_argument('--lastMinutes', '-m', help="Last minutes to get data for", dest='last_minutes', type=int, default=5, required=False)
     parser.add_argument('--host', '-a', help="ElasticSearch Host's ip/address", type=str, default='elastic.monitor.net', required=False)
     parser.add_argument('--port', '-p', help="ElasticSearch Host's port", type=int, default=9200, required=False)
     parser.add_argument('--user', '-u', help="ElasticSearch username", type=str, default='elastic', required=False)
